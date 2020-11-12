a8ea3fb979c8cc48e6f00ada15bcd5887f8cc5aa
Gabriel Ortelan <gabriel.montanari@bandtec.com.br>
2020-11-12 19:32:52 -0300

    adicionei tudo

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function scrollHistoria() {
+  let scroll = document.getElementById("secHistoria");
+  scroll.scrollIntoView();
+}
+
+function scrollHabilidade() {
+  let scroll = document.getElementById("secHabilidades");
+  scroll.scrollIntoView;
+}
