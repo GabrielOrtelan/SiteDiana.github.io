82095e6248eaa65fa1d91d37a4d05557f9e055b3
GabrielOrtelan <gabriel.montanari@bandtec.com.br>
2020-11-22 21:55:25 -0300

    varias coisas

--- js/index.js
+++ js/index.js
@@ -7,3 +7,42 @@ function scrollHabilidade() {
   let scroll = document.getElementById("secHabilidades");
   scroll.scrollIntoView;
 }
+
+
+function abrirPassiva(){
+  descP.style.display = "block";
+  descQ.style.display = "none";
+  descW.style.display = "none"
+  habilidadeP.style.display = "block";
+  habilidadeQ.style.display = "none";
+  habilidadeW.style.display = "none";
+  habilidadeE.style.display = "none";
+  habilidadeR.style.display = "none";
+
+}
+
+function abrirQ(){
+  descP.style.display = "none";
+  descQ.style.display = "block";
+  descW.style.display = "none"
+  habilidadeP.style.display = "none";
+  habilidadeQ.style.display = "block";
+  habilidadeW.style.display = "none";
+  habilidadeE.style.display = "none";
+  habilidadeR.style.display = "none";
+
+}
+
+function abrirW(){
+  descP.style.display = "none";
+  descQ.style.display = "none";
+  descW.style.display = "block"
+  habilidadeP.style.display = "none";
+  habilidadeQ.style.display = "none";
+  habilidadeW.style.display = "block";
+  habilidadeE.style.display = "none";
+  habilidadeR.style.display = "none";
+
+}
+
+
